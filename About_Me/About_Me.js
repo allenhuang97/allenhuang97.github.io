9a64e0229e8ed08f3ea5813c9f2cf8c23849750e
allenhuang97 <allen.weixi.huang@gmail.com>
2016-01-01 17:03:54 -0500

    My Projects - Pacman Included
    Created Page and Carousel for Pacman Project using "slick"
    Include resources for Pacman Pages
    Small Decorative changes to other pages

--- About_Me/About_Me.js
+++ About_Me/About_Me.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+<script type="text/javascript" src="../js/jssor.slider.mini.js"></script>
+<script>
+    jQuery(document).ready(function ($) {
+        var options = {
+            
+        };
+            
+        var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+        
+    });
+</script>
---
+++ My Projects/Pacman/Project_Pacman.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	"use strict";
+	$('.carousel').slick({
+  		centerPadding: '60px',
+  		dots: true,
+		adaptiveHeight: true,
+        slidesToShow: 1,
+	});
+});
