9a432f91220d21e23a943be6cc4353d1988f294a
allenhuang97 <allen.weixi.huang@gmail.com>
2016-01-17 11:39:34 -0500

    Include About Me Page

--- About_Me/About_Me.js
+++ About_Me/About_Me.js
@@ -1,12 +1,6 @@
-// JavaScript Document
-<script type="text/javascript" src="../js/jssor.slider.mini.js"></script>
-<script>
-    jQuery(document).ready(function ($) {
-        var options = {
-            
-        };
-            
-        var jssor_slider1 = new $JssorSlider$("slider1_container", options);
-        img
-    });
-</script>
+$(document).ready(function(){
+	"use strict";
+	$('.about_me').animate({
+		opacity: 1
+	}, 1000);
+});
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 	"use strict";
-	console.log("teest");
 	$('.Home_Page').animate({
 		opacity: 1
 	}, 1000);
