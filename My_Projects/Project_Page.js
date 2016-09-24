b63684fc1af73cce7de1825999e9c6aaf600e53b
allenhuang97 <allen.weixi.huang@gmail.com>
2016-09-24 14:14:40 -0400

    Updating Project Page Visuals

--- My_Projects/Project_Page.js
+++ My_Projects/Project_Page.js
@@ -1,11 +1,11 @@
+var WhatBrokeAnimate = 0;
 var PacmanAnimate = 0;
+var rspatiotempAnimate = 0;
+var charRecogAnimate = 0;
 var TowerAnimate = 0;
 
 $(document).ready(function() {
     "use strict";
-    $('.Project_Page').animate({
-        opacity: 1
-    }, 1000);
     $('#Pacman-Block').mouseenter(function() {
         if ($('#Pacman-Block').is(':animated')) {} else {
             PacmanAnimate = 0;
@@ -15,16 +15,59 @@ $(document).ready(function() {
         }
     });
     $('#Pacman-Block').mouseleave(function() {
-        console.log(PacmanAnimate);
-
         if (PacmanAnimate === 0) {
-            console.log("Run");
             PacmanAnimate = 1;
-            console.log("After: " + PacmanAnimate);
             $(this).animate({
                 width: '-=10px',
             });
-
+        }
+    });
+    $('#WhatBroke-Block').mouseenter(function() {
+        if ($('#WhatBroke-Block').is(':animated')) {} else {
+            WhatBrokeAnimate = 0;
+            $(this).animate({
+                width: '+=10px'
+            });
+        }
+    });
+    $('#WhatBroke-Block').mouseleave(function() {
+        if (WhatBrokeAnimate === 0) {
+            WhatBrokeAnimate = 1;
+            $(this).animate({
+                width: '-=10px',
+            });
+        }
+    });
+    $('#rspatiotemp-Block').mouseenter(function() {
+        if ($('#rspatiotemp-Block').is(':animated')) {} else {
+            rspatiotempAnimate = 0;
+            $(this).animate({
+                width: '+=10px'
+            });
+        }
+    });
+    $('#rspatiotemp-Block').mouseleave(function() {
+        if (rspatiotempAnimate === 0) {
+            rspatiotempAnimate = 1;
+            $(this).animate({
+                width: '-=10px'
+            });
+        }
+    });
+    $('#charRecog-Block').mouseenter(function() {
+        if ($('#charRecog-Block').is(':animated')) {} else {
+            charRecogAnimate = 0;
+            $(this).animate({
+                width: '+=10px'
+            });
+        }
+    });
+    $('#charRecog-Block').mouseleave(function() {
+        if (charRecogAnimate === 0) {
+            charRecogAnimate = 1;
+            $(this).animate({
+                width: '-=10px'
+            });
         }
     });
     $('#Tower-Defense-Block').mouseenter(function() {
