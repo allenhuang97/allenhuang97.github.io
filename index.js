cb08fb96b63e44bb1ffdcbb05c52de062a8b7900
allenhuang97 <allen.weixi.huang@gmail.com>
2017-06-26 22:54:41 -0400

    minor style changes

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
-        window.location.hash = hash;
       });
     }
   });
